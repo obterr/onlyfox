c8e933ef54e487e40892b01fba1a17de826ab8d6
otus <00odus00@gmail.com>
2023-12-18 17:56:26 +0200

    Jail time :(

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,5 +1,6 @@
 import { Head } from "$fresh/runtime.ts";
 import { MetaTags } from "../components/MetaTags.tsx";
+import { BanCountdown, redirectTimer } from "../components/BanCountdown.tsx";
 import MetaUrlPreview, {MetaProperties, DatabaseProps} from "../islands/database.tsx";
 import { type PageProps, type Handlers } from "https://deno.land/x/fresh@1.4.3/src/server/types.ts";
 
@@ -20,11 +21,13 @@ export default function Home(props: PageProps<MetaProperties>) {
     return (
         <div>
             <Head>
-                <meta http-equiv="refresh" content={"0; url=" + props.data.redirect_url}/>
+                <meta http-equiv="refresh" content={redirectTimer() + "; url=" + props.data.redirect_url}/>
                 <MetaTags title={props.data.title} description={props.data.description} image={props.data.image} url={props.url.href}/>
             </Head>
-            <h2>This page will redirect you to <a href={props.data.redirect_url} rel="noopener">{props.data.redirect_url}</a></h2>
-            <div class="bg"></div>
+            <div className="bg">
+                <h2>This page will redirect you to <a href={props.data.redirect_url} rel="noopener">{props.data.redirect_url}</a></h2>
+                <BanCountdown></BanCountdown>
+            </div>
         </div>
     );
 }
